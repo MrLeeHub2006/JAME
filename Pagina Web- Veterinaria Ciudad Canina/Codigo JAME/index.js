90d85a851f4dc1491a20d5117f928f485eee5847
MrLeeHub2006 <misterlee272006@gmail.com>
2024-06-06 17:40:15 -0500

    Actualizacion del main y creacion del loginin

---
+++ Pagina Web- Veterinaria Ciudad Canina/Codigo JAME/index.js
@@ -0,0 +1,12 @@
+function tema(){
+    var index = document.getElementById('html');
+    var icon = document.getElementById('botont');
+        if (index.getAttribute("data-bs-theme") === "light") {
+          index.setAttribute("data-bs-theme", "dark");
+          icon.setAttribute("class","bi bi-moon-fill")
+        }
+        else {
+          index.setAttribute("data-bs-theme", "light");
+          icon.setAttribute("class","bi bi-sun-fill")
+        }
+      }
