af5319b0eb0c2c9ceb3ca02f6c2f1d2d3cf51209
MrLeeHub2006 <125906456+MrLeeHub2006@users.noreply.github.com>
2024-08-16 13:24:00 -0500

    reparacion y agregacion del banner de iniciio

--- Pagina Web- Veterinaria Ciudad Canina/Jame codigo2/COSMETICS/login.php
+++ Pagina Web- Veterinaria Ciudad Canina/Jame codigo2/COSMETICS/login.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/login.css">
-    <title>Inicia Sesión</title>
+    <title>Inicia Sesión Veterinaria Ciudad Canina</title>
 </head>
 <body>
     <main>
