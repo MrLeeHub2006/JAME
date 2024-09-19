412bb39ec6a9d7a8b0edc684004209c054871033
DiegoEsteban10 <164409835+DiegoEsteban10@users.noreply.github.com>
2024-09-19 11:47:45 -0500

    actuaqlizacion de img-carousel-colores

--- COSMETICS/index.php
+++ COSMETICS/index.php
@@ -19,6 +19,13 @@ try {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 <head>
 
     <meta charset="UTF-8">
@@ -31,41 +38,106 @@ try {
     <link rel="icon" href="/img/logovet.png">
 </head>
 <body>
-    <header>
-            <div class="texto-rosa-header">
-                <p>El secreto de la belleza es ser tú misma</p>
+<header>
+        <div class="texto-rosa-header">
+            <p>La mejor opcion para el cuidado de tu mascota</p>
+        </div>
+        <div class="logo-account-navbar">
+            <div class="logo">
+                <a href="index.php"><img src="IMG/logovet.png" id="logo-header"></a>
             </div>
-            <div class="logo-account-navbar">
-                <div class="logo">
-                    <a href="index.php"><img src="IMG/Logo_Paradise_cosmetics.png" id="logo-header"></a>
-                </div>
-                <div class="nav-search">
-                    <input type="text" id="search-input" placeholder="Buscar productos...">
-                    <button id="search-button"><i class="fa-solid fa-search"></i></button>
-                </div>
+            <div class="nav-search">
+                <input type="text" id="search-input" placeholder="Buscar productos...">
+                <button id="search-button"><i class="fa-solid fa-search"></i></button>
+            </div>
 
-                <ul class="nav-account">
-                    <li><a href="<?php if (isset($usuario)){echo 'perfil-usuario.php?id='.$id.'';} else if (!isset($usuario)){echo 'login.php'; };?>"><i class="fa-regular fa-user"></i> <?php if (isset($usuario)){echo 'MI CUENTA';} else if (!isset($usuario)){echo 'Invitado(uknown)'; };?></a></li>
-                    <li><a href="index.php#carritox"><i class="fa-solid fa-cart-shopping"></i> CARRITO</a></li>
-                </ul>
-            </div>
-            <div class="texto-rosa-header nav-list-rosa">
-                <ul class="nav-list">
-                    <li class="nav-list-item"><a href="index.php">Inicio</a></li>
-                    <li class="nav-list-item"><a href="index.php#productos">Productos</a></li>
-                    <li class="nav-list-item"><a href="index.php#about-us">Acerca de nosotros</a></li>
-                    <li class="nav-list-item"><a href="index.php#contact-info">Contacto</a></li>
-                </ul>
-            </div>
+            <ul class="nav-account">
+                <li><a
+                        href="<?php if (isset($usuario)) {
+                            echo 'perfil-usuario.php?id=' . $id . '';
+                        } else if (!isset($usuario)) {
+                            echo 'login.php';
+                        }
+                        ; ?>"><i
+                            class="fa-regular fa-user"></i>
+                        <?php if (isset($usuario)) {
+                            echo 'MI CUENTA';
+                        } else if (!isset($usuario)) {
+                            echo 'Ingresar';
+                        }
+                        ; ?></a>
+                </li>
+                <li><a href="index.php#carritox"><i class="fa-solid fa-cart-shopping"></i> CARRITO</a></li>
+            </ul>
+        </div>
+    
+        <nav class="navbar navbar-expand-lg navbar-light  ">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">    
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php#productos">Productos</a>
+      </li>
+              <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                        Servicios
+          </a>
+          <ul class="dropdown-menu dropdown-menu-dark">
+            <li><a class="dropdown-item" href="#">Grooming</a></li>
+            <li><a class="dropdown-item" href="#">Medicina</a></li>
+            <li><a class="dropdown-item" href="#">Consultas</a></li>
+          </ul>
+        </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="index.php#contact-info">Contactanos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php#about-us">Acerca de nosotros</a>
+      </li>
+    </ul>
+  </div>
+</nav>
     </header>
 
     <main>
-        <section class="section-introduction">
-            <div class="text-introduction">
-                <h1>Bienvenido a Tu Tienda de Cosméticos</h1>
-                <p>Descubre una amplia gama de productos de belleza y cuidado personal</p>
-                <a href="index.php#productos"><button class="btn-introduction">Ver Productos</button></a>
+    <div id="carouselExampleIndicators" class="carousel slide position-relative" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="/IMG/Banner1.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="/IMG/Banner1.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="/IMG/Banner1.png" class="d-block w-100" alt="...">
+                </div>
             </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+  <div class="position-absolute top-50 end-0 translate-middle">
+    <h1>Bienvenido a Veterinaria Ciudad Canina</h1>
+    <p>Descubre la mejor gama de cuidado y servicios para la mascota</p>
+    <a href="index.php#nosotros"><button class="btn-introduction">Conocer más</button></a>
+  </div>
+</div>
             
         <a name="productos"></a>
         </section>
@@ -280,5 +352,15 @@ try {
         });
     </script>
 </body>
+<!-- Optional JavaScript; choose one of the two! -->
 
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
 </html>
