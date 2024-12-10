3b90e90c7aadc77cfe43876e1640ab529f3d043f
Jjuparela2007 <163196035+elaserrot@users.noreply.github.com>
2024-12-09 23:28:07 -0500

    se modifican los iconos , de cada consulta
    iconos actualizados

--- JAME/front/src/pages/Cliente/ServiciosMedicina.jsx
+++ JAME/front/src/pages/Cliente/ServiciosMedicina.jsx
@@ -19,11 +19,12 @@ export default function Servicios() {
                 <div class="col-md-6 col-lg">
                     <div class="card h-100 text-center bg-white">
                         <div class="card-body">
-                            <svg class="paw-icon w-50" viewBox="0 0 24 24" fill="#000000FF">
-                                <path d="M12,8L10.67,8.09C9.81,7.07 7.4,4.5 5,4.5C5,4.5 3.03,7.46 4.96,11.41C4.41,12.24 4.07,12.67 4,13.66L2.07,13.95L2.28,14.93L4.04,14.67L4.18,15.38L2.61,16.32L3.08,17.21L4.53,16.32C5.68,18.76 8.59,20 12,20C15.41,20 18.32,18.76 19.47,16.32L20.92,17.21L21.39,16.32L19.82,15.38L19.96,14.67L21.72,14.93L21.93,13.95L20,13.66C19.93,12.67 19.59,12.24 19.04,11.41C20.97,7.46 19,4.5 19,4.5C16.6,4.5 14.19,7.07 13.33,8.09L12,8Z"/>
-                            </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                         viewBox="0 0 1500 505">
+                            <path d="M192 48c0-26.5 21.5-48 48-48L400 0c26.5 0 48 21.5 48 48l0 464-80 0 0-80c0-26.5-21.5-48-48-48s-48 21.5-48 48l0 80-80 0 0-464zM48 96l112 0 0 416L48 512c-26.5 0-48-21.5-48-48L0 320l80 0c8.8 0 16-7.2 16-16s-7.2-16-16-16L0 288l0-64 80 0c8.8 0 16-7.2 16-16s-7.2-16-16-16L0 192l0-48c0-26.5 21.5-48 48-48zm544 0c26.5 0 48 21.5 48 48l0 48-80 0c-8.8 0-16 7.2-16 16s7.2 16 16 16l80 0 0 64-80 0c-8.8 0-16 7.2-16 16s7.2 16 16 16l80 0 0 144c0 26.5-21.5 48-48 48l-112 0 0-416 112 0zM312 64c-8.8 0-16 7.2-16 16l0 24-24 0c-8.8 0-16 7.2-16 16l0 16c0 8.8 7.2 16 16 16l24 0 0 24c0 8.8 7.2 16 16 16l16 0c8.8 0 16-7.2 16-16l0-24 24 0c8.8 0 16-7.2 16-16l0-16c0-8.8-7.2-16-16-16l-24 0 0-24c0-8.8-7.2-16-16-16l-16 0z"/></svg>
+                            <p class="card-text mb-4"></p>
                             <h5 class="card-title mb-3">Medicina General</h5>
-                            <p class="card-text mb-4">Consulta de medicina veterinaria general para caninos y felinos menores de 7 años.</p>
+                            <p class="card-text mb-5">Consulta de medicina veterinaria general para caninos y felinos menores de 7 años.</p>
                             <button class="btn btn-outline-primary">Agendar cita</button>
                         </div>
                     </div>
@@ -33,9 +34,10 @@ export default function Servicios() {
                 <div class="col-md-6 col-lg">
                     <div class="card h-100 text-center bg-white"> 
                         <div class="card-body">
-                            <svg class="paw-icon w-50" viewBox="0 0 24 24" fill="#000000FF">
-                                <path d="M12,8L10.67,8.09C9.81,7.07 7.4,4.5 5,4.5C5,4.5 3.03,7.46 4.96,11.41C4.41,12.24 4.07,12.67 4,13.66L2.07,13.95L2.28,14.93L4.04,14.67L4.18,15.38L2.61,16.32L3.08,17.21L4.53,16.32C5.68,18.76 8.59,20 12,20C15.41,20 18.32,18.76 19.47,16.32L20.92,17.21L21.39,16.32L19.82,15.38L19.96,14.67L21.72,14.93L21.93,13.95L20,13.66C19.93,12.67 19.59,12.24 19.04,11.41C20.97,7.46 19,4.5 19,4.5C16.6,4.5 14.19,7.07 13.33,8.09L12,8Z"/>
-                            </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 1500 505">
+                            <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-96 55.2C54 332.9 0 401.3 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7c0-81-54-149.4-128-171.1l0 50.8c27.6 7.1 48 32.2 48 62l0 40c0 8.8-7.2 16-16 16l-16 0c-8.8 0-16-7.2-16-16s7.2-16 16-16l0-24c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 24c8.8 0 16 7.2 16 16s-7.2 16-16 16l-16 0c-8.8 0-16-7.2-16-16l0-40c0-29.8 20.4-54.9 48-62l0-57.1c-6-.6-12.1-.9-18.3-.9l-91.4 0c-6.2 0-12.3 .3-18.3 .9l0 65.4c23.1 6.9 40 28.3 40 53.7c0 30.9-25.1 56-56 56s-56-25.1-56-56c0-25.4 16.9-46.8 40-53.7l0-59.1zM144 448a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"/></svg>
+                            <p class="card-text mb-3"></p>
                             <h5 class="card-title mb-3">Consulta Nutricional</h5>
                             <p class="card-text mb-4">Recomendaciones sobre comidas balanceadas respecto a sus nutrientes ajusta para los perros y gatos.</p>
                             <button class="btn btn-outline-primary">Agendar cita</button>
@@ -47,11 +49,12 @@ export default function Servicios() {
                 <div class="col-md-6 col-lg">
                     <div class="card h-100 text-center bg-white">
                         <div class="card-body">
-                            <svg class="paw-icon w-50" viewBox="0 0 24 24" fill="#000000FF">
-                                <path d="M12,8L10.67,8.09C9.81,7.07 7.4,4.5 5,4.5C5,4.5 3.03,7.46 4.96,11.41C4.41,12.24 4.07,12.67 4,13.66L2.07,13.95L2.28,14.93L4.04,14.67L4.18,15.38L2.61,16.32L3.08,17.21L4.53,16.32C5.68,18.76 8.59,20 12,20C15.41,20 18.32,18.76 19.47,16.32L20.92,17.21L21.39,16.32L19.82,15.38L19.96,14.67L21.72,14.93L21.93,13.95L20,13.66C19.93,12.67 19.59,12.24 19.04,11.41C20.97,7.46 19,4.5 19,4.5C16.6,4.5 14.19,7.07 13.33,8.09L12,8Z"/>
-                            </svg>
-                            <h5 class="card-title mb-3">Vacunación</h5>
-                            <p class="card-text mb-4">Guías para la vacunación de perros (caninos) y gatos (felinos).</p>
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                         viewBox="0 0 1500 505">
+                            <path d="M441 7l32 32 32 32c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-15-15L417.9 128l55 55c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-72-72L295 73c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l55 55L422.1 56 407 41c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0zM210.3 155.7l61.1-61.1c.3 .3 .6 .7 1 1l16 16 56 56 56 56 16 16c.3 .3 .6 .6 1 1l-191 191c-10.5 10.5-24.7 16.4-39.6 16.4l-88.8 0L41 505c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l57-57 0-88.8c0-14.9 5.9-29.1 16.4-39.6l43.3-43.3 57 57c6.2 6.2 16.4 6.2 22.6 0s6.2-16.4 0-22.6l-57-57 41.4-41.4 57 57c6.2 6.2 16.4 6.2 22.6 0s6.2-16.4 0-22.6l-57-57z"/></svg>
+                            <p class="card-text mb-3"></p>
+                            <h5 class="card-title mb-4">Vacunación</h5>
+                            <p class="card-text mb-5">Guías para la vacunación de perros (caninos) y gatos (felinos).</p>
                             <button class="btn btn-outline-primary">Agendar cita</button>
                         </div>
                     </div>
@@ -61,9 +64,10 @@ export default function Servicios() {
                 <div class="col-md-6 col-lg">
                     <div class="card h-100 text-center bg-white">
                         <div class="card-body">
-                            <svg class="paw-icon  w-50" viewBox="0 0 24 24" fill="#000000FF">
-                                <path d="M12,8L10.67,8.09C9.81,7.07 7.4,4.5 5,4.5C5,4.5 3.03,7.46 4.96,11.41C4.41,12.24 4.07,12.67 4,13.66L2.07,13.95L2.28,14.93L4.04,14.67L4.18,15.38L2.61,16.32L3.08,17.21L4.53,16.32C5.68,18.76 8.59,20 12,20C15.41,20 18.32,18.76 19.47,16.32L20.92,17.21L21.39,16.32L19.82,15.38L19.96,14.67L21.72,14.93L21.93,13.95L20,13.66C19.93,12.67 19.59,12.24 19.04,11.41C20.97,7.46 19,4.5 19,4.5C16.6,4.5 14.19,7.07 13.33,8.09L12,8Z"/>
-                            </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                         viewBox="0 0 1500 505">
+                           <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9l2.6-2.4C267.2 438.6 256 404.6 256 368c0-97.2 78.8-176 176-176c28.3 0 55 6.7 78.7 18.5c.9-6.5 1.3-13 1.3-19.6l0-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1l0 5.8c0 41.5 17.2 81.2 47.6 109.5zM432 512a144 144 0 1 0 0-288 144 144 0 1 0 0 288zm47.9-225c4.3 3.7 5.4 9.9 2.6 14.9L452.4 356l35.6 0c5.2 0 9.8 3.3 11.4 8.2s-.1 10.3-4.2 13.4l-96 72c-4.5 3.4-10.8 3.2-15.1-.6s-5.4-9.9-2.6-14.9L411.6 380 376 380c-5.2 0-9.8-3.3-11.4-8.2s.1-10.3 4.2-13.4l96-72c4.5-3.4 10.8-3.2 15.1 .6z"/></svg>
+                            <p class="card-text mb-3"></p>
                             <h5 class="card-title mb-3">Cardiología</h5>
                             <p class="card-text mb-4">Consulta especializada de cardiología veterinaria para seguimiento y control del paciente.</p>
                             <button class="btn btn-outline-primary">Agendar cita</button>
@@ -75,10 +79,11 @@ export default function Servicios() {
                 <div class="col-md-6 col-lg">
                     <div class="card h-100 text-center bg-white">
                         <div class="card-body">
-                            <svg class="paw-icon w-50" viewBox="0 0 24 24" fill="#000000FF">
-                                <path d="M12,8L10.67,8.09C9.81,7.07 7.4,4.5 5,4.5C5,4.5 3.03,7.46 4.96,11.41C4.41,12.24 4.07,12.67 4,13.66L2.07,13.95L2.28,14.93L4.04,14.67L4.18,15.38L2.61,16.32L3.08,17.21L4.53,16.32C5.68,18.76 8.59,20 12,20C15.41,20 18.32,18.76 19.47,16.32L20.92,17.21L21.39,16.32L19.82,15.38L19.96,14.67L21.72,14.93L21.93,13.95L20,13.66C19.93,12.67 19.59,12.24 19.04,11.41C20.97,7.46 19,4.5 19,4.5C16.6,4.5 14.19,7.07 13.33,8.09L12,8Z"/>
-                            </svg>
-                            <h5 class="card-title mb-3">Esterilización</h5>
+                        <svg xmlns="http://www.w3.org/2000/svg" 
+                        viewBox="0 0 1500 505">
+                            <path d="M269.4 2.9C265.2 1 260.7 0 256 0s-9.2 1-13.4 2.9L54.3 82.8c-22 9.3-38.4 31-38.3 57.2c.5 99.2 41.3 280.7 213.6 363.2c16.7 8 36.1 8 52.8 0C454.7 420.7 495.5 239.2 496 140c.1-26.2-16.3-47.9-38.3-57.2L269.4 2.9zM160.9 286.2c4.8 1.2 9.9 1.8 15.1 1.8c35.3 0 64-28.7 64-64l0-64 44.2 0c12.1 0 23.2 6.8 28.6 17.7L320 192l64 0c8.8 0 16 7.2 16 16l0 32c0 44.2-35.8 80-80 80l-48 0 0 50.7c0 7.3-5.9 13.3-13.3 13.3c-1.8 0-3.6-.4-5.2-1.1l-98.7-42.3c-6.6-2.8-10.8-9.3-10.8-16.4c0-2.8 .6-5.5 1.9-8l15-30zM160 160l40 0 8 0 0 32 0 32c0 17.7-14.3 32-32 32s-32-14.3-32-32l0-48c0-8.8 7.2-16 16-16zm128 48a16 16 0 1 0 -32 0 16 16 0 1 0 32 0z"/></svg>
+                            <p class="card-text mb-4"></p>
+                            <h5 class="card-title mb-5">Esterilización</h5>
                             <p class="card-text mb-4">Ofrecemos servicios de esterilización y castración para perros y gatos.</p>
                             <button class="btn btn-outline-primary">Agendar cita</button>
                         </div>
