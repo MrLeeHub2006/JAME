9397c82750a97ecd8254703cecf2c10c113def4c
DiegoEsteban10 <egoessanrero@gmail.com>
2024-12-11 11:07:41 -0500

    implementacion de las cors

--- JAME/back/app/Http/Middleware/Cors.php
+++ JAME/back/app/Http/Middleware/Cors.php
@@ -4,17 +4,22 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class Cors
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')  // Permite todas las fuentes, o usa una URL específica como 'http://localhost:5173'
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With')
+            ->header('Access-Control-Allow-Credentials', 'true');
     }
 }
--- JAME/back/routes/api.php
+++ JAME/back/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Middleware\Cors;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,11 @@ use App\Http\Controllers\Auth\LoginController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::middleware([Cors::class])->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    // otras rutas...
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
